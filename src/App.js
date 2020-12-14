3cd7643cd9892769292b0c11870506614c6f3e1d
ankit <ankit.patidar@daffodilsw.com>
2020-12-14 12:42:42 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ function App() {
   return (
     <div className="App" style={{ display: "flex", justifyContent: "center", alignItems: 'center' }}>
       <header className="App-header">
-        Ankit Patidar
+        <h1>Ankit Patidar</h1>
+        <h3>Flutter developer | React Js |Core Java </h3>
       </header>
     </div>
   );
