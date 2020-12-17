86c9166d6b5b7b0fad737ae0046139a888277df4
ankit <ankit.patidar@daffodilsw.com>
2020-12-17 13:04:34 +0530

    initial commit

--- src/topics/defination/NodeDef.js
+++ src/topics/defination/NodeDef.js
@@ -5,7 +5,7 @@ function NodeDef() {
         <div className='container'>
             <h3 className={Style.h3}>What is Node Js ?</h3>
             <p>Node.js is a cross-platform environment and library for running JavaScript applications which is used to create networking and server-side applications.</p>
-
+            <p>Node.js: JavaScript run-time environment that executes JavaScript code outside of a browser (such as a server).</p>
         </div>
     )
 }
